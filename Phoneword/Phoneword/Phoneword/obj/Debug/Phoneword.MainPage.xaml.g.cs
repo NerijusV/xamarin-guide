bcc0601def464c7d41c38d0c4dfafec8f3c16247
NerijusV <nerijus.vaiciukynas@ecm4d.com>
2017-04-27 02:06:42 +0300

    ko

--- Phoneword/Phoneword/Phoneword/obj/Debug/Phoneword.MainPage.xaml.g.cs
+++ Phoneword/Phoneword/Phoneword/obj/Debug/Phoneword.MainPage.xaml.g.cs
@@ -17,8 +17,24 @@ namespace Phoneword {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Entry phoneNumberText;
+        
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Button translateButon;
+        
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Button callButton;
+        
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Button callHistoryButton;
+        
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             this.LoadFromXaml(typeof(MainPage));
+            phoneNumberText = this.FindByName<global::Xamarin.Forms.Entry>("phoneNumberText");
+            translateButon = this.FindByName<global::Xamarin.Forms.Button>("translateButon");
+            callButton = this.FindByName<global::Xamarin.Forms.Button>("callButton");
+            callHistoryButton = this.FindByName<global::Xamarin.Forms.Button>("callHistoryButton");
         }
     }
 }
